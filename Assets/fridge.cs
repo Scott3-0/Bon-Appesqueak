7df2b1617e1a69fde6dd2d3a227fa1201bd8ae78
Ori Friesen <32725393+orifriesen@users.noreply.github.com>
2020-02-06 11:24:47 -0800

    Added fridge, toaster, and tables

--- Assets/fridge.cs
+++ Assets/fridge.cs
@@ -7,7 +7,6 @@ public class fridge : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start!");
     }
 
     // Update is called once per frame
@@ -18,6 +17,6 @@ public class fridge : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("I collided!");
+        Debug.Log("fridge");
     }
 }
