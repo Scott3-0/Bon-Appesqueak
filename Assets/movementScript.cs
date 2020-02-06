20d0855586a466e442537df8eeeb7948990293c3
Mateo Stange <stangemateo@gmail.com>
2020-02-06 10:10:40 -0800

    rotation script
    helohelhd
    saf

--- Assets/movementScript.cs
+++ Assets/movementScript.cs
@@ -5,28 +5,38 @@ using UnityEngine;
 public class movementScript : MonoBehaviour
 {
     public float panSpeed = 20f;
+    public float rotSpeed = .01f;
 
     void Update()
     {
         Vector3 pos = transform.position;
 
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.rotation = Quaternion.LookRotation(movement);
+
         if (Input.GetKey("w"))
         {
             pos.z += panSpeed * Time.deltaTime;
+            transform.Translate(movement * rotSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey("s"))
         {
             pos.z -= panSpeed * Time.deltaTime;
+            transform.Translate(movement * rotSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey("d"))
         {
             pos.x += panSpeed * Time.deltaTime;
+            transform.Translate(movement * rotSpeed * Time.deltaTime, Space.World);
         } 
         if (Input.GetKey("a"))
         {
             pos.x -= panSpeed * Time.deltaTime;
+            transform.Translate(movement * rotSpeed * Time.deltaTime, Space.World);
         }
-
 
         transform.position = pos;
     }
