0c4a59aed21d488c2d049d5d49185cf93f2f8494
Mateo Stange <stangemateo@gmail.com>
2020-02-12 10:39:37 -0800

    mvp worthy movement shite
    this uses rays to reduce bumping into things and breaking everything. uses vertical and horizontal inputs

--- Assets/movementScript.cs
+++ Assets/movementScript.cs
@@ -4,40 +4,41 @@ using UnityEngine;
 
 public class movementScript : MonoBehaviour
 {
-    public float panSpeed = 20f;
-    public float rotSpeed = .01f;
+    public float speed = 50;
+    public float Between;
+    float turnspeed = 150f;
+
+    bool canGo = true;
 
     void Update()
     {
         Vector3 pos = transform.position;
+        RaycastHit hit;
+        Ray myRay = new Ray(transform.position, transform.forward);
+        float distance = 1f;
+        //use raycasts here. check max distance, use a bool
 
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.rotation = Quaternion.LookRotation(movement);
-
-        if (Input.GetKey("w"))
+        float mov = Input.GetAxis("Vertical");
+        if(canGo == true)
         {
-            pos.z += panSpeed * Time.deltaTime;
-            transform.Translate(movement * rotSpeed * Time.deltaTime, Space.World);
-        }
-        if (Input.GetKey("s"))
-        {
-            pos.z -= panSpeed * Time.deltaTime;
-            transform.Translate(movement * rotSpeed * Time.deltaTime, Space.World);
-        }
-        if (Input.GetKey("d"))
-        {
-            pos.x += panSpeed * Time.deltaTime;
-            transform.Translate(movement * rotSpeed * Time.deltaTime, Space.World);
-        } 
-        if (Input.GetKey("a"))
-        {
-            pos.x -= panSpeed * Time.deltaTime;
-            transform.Translate(movement * rotSpeed * Time.deltaTime, Space.World);
+            gameObject.transform.Translate(Vector3.forward * mov * Time.deltaTime * speed);
         }
 
-        transform.position = pos;
+        gameObject.transform.Rotate(0, Input.GetAxis("Horizontal") * turnspeed * Time.deltaTime, 0);
+
+        Debug.DrawRay(transform.position, transform.forward, Color.green);
+
+        if (Physics.Raycast(myRay, out hit, distance)) 
+        {
+            canGo = false;
+            Debug.Log("Hit");
+            Between = hit.distance;
+        }
+        else
+        {
+            canGo = true;
+        }
+
+
     }
 }
