e548803540c6c4a63e3e82afd4eacb96dc8cb496
Nathaniel <nnelson5302@gmail.com>
2020-02-14 11:24:55 -0800

    Toaster minigame works in its basic form

---
+++ Assets/ToastTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ToastTimer : MonoBehaviour
+{
+    double timeLeft = 5.0f;
+    double timerTime;
+
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = 5.0f;
+        timerTime = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timerTime = Math.Round(timeLeft, 2);
+        timerText.text = timerTime.ToString();
+        if (timeLeft<=0)
+        {
+
+        }
+    }
+}
