604eb2021ecf33637514510b67c25f973d44d0db
Nathaniel <nnelson5302@gmail.com>
2020-02-21 10:19:20 -0800

    Finished Making Order Slips

--- Assets/OrderSlips.cs
+++ Assets/OrderSlips.cs
@@ -6,16 +6,44 @@ using UnityEngine.UI;
 public class OrderSlips : MonoBehaviour
 {
     public Text orderText;
-    string[] orders;
+
+    List<string> orders = new List<string>();
+    int orderNumber = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        orders.Add("Order 1: Toast");
+        orders.Add("Order 2: Toast");
+        orders.Add("Order 3: Toast w/ Butter");
     }
 
     // Update is called once per frame
     void Update()
     {
-        orders
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if ((orderNumber+1) < orders.Count)
+            {
+                orderNumber++;
+            }
+            else
+            {
+                orderNumber = 0;
+            }
+            orderText.text = orders[orderNumber];
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (orderNumber > 0)
+            {
+                orderNumber--;
+            }
+            else
+            {
+                orderNumber = (orders.Count -1);
+            }
+            orderText.text = orders[orderNumber];
+        }
     }
 }
