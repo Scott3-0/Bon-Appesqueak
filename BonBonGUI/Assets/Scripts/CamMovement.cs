06ae607c916b3829df9ace9330921cf328ad3ed6
Scott 3.0 <bremmerscottw@gmail.com>
2020-03-02 11:24:29 -0800

    welp

---
+++ BonBonGUI/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    public GameObject target;
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.rotation = new Quaternion(90f, 0f, 0f, 0f);
+        gameObject.transform.position = target.transform.position + new Vector3(0f, 10f, 0f);
+        transform.LookAt(target.transform);
+    }
+}
